ddc4334d64e377f7557a4e2a12319fefe0de7e7b
Дмитрий Карих <the.dr.hax@gmail.com>
2020-07-21 02:26:22 +0300

    data.js: Очищать кэш при ошибке

--- src/js/data.js
+++ src/js/data.js
@@ -5,14 +5,24 @@ class Data {
 
   static async segments() {
     if (Data._segments == null) {
-      Data._segments = fetch('/data/segments.json').then((res) => res.json());
+      Data._segments = fetch('/data/segments.json')
+        .then((res) => res.json())
+        .catch((ex) => {
+          Data._segments = null;
+          throw ex;
+        });
     }
     return Data._segments;
   }
 
   static async categories() {
     if (Data._categories == null) {
-      Data._categories = fetch('/data/categories.json').then((res) => res.json());
+      Data._categories = fetch('/data/categories.json')
+        .then((res) => res.json())
+        .catch((ex) => {
+          Data._categories = null;
+          throw ex;
+        });
     }
     return Data._categories;
   }
